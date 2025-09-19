bd9e91ab25fbdf40ad5b778d51dfe98163f14f04
Nicolo Massari <nmassari@ethz.ch>
2025-09-19 08:17:11 +0200

    fix: add show logs

--- build.R
+++ build.R
@@ -7,11 +7,11 @@ deploy_quiz <- function(module_name) {
   # Deploy directly from module directory - no temp directories!
   module_dir <- file.path("modules", module_name)
   rmd_file <- file.path(module_dir, paste0(module_name, ".Rmd"))
-  
+
   if (!file.exists(rmd_file)) {
     stop("Quiz file not found: ", rmd_file)
   }
-  
+
   # Deploy directly from module directory
   rsconnect::deployDoc(
     doc = rmd_file,
@@ -19,6 +19,7 @@ deploy_quiz <- function(module_name) {
     forceUpdate = TRUE,
     logLevel = "verbose"
   )
+  rsconnect::showLogs()
 }
 
 
