9e3a2541eccb4accd49f78927a9101e6d18aadeb
Nicolo Massari <nmassari@ethz.ch>
2025-09-23 11:27:36 +0200

    feat: move common module files to common modules/common to have one source of truth

--- build.R
+++ build.R
@@ -4,12 +4,26 @@ source("config.R")
 # HELPER
 
 deploy_quiz <- function(module_name) {
-  # Deploy directly from module directory - no temp directories!
   module_dir <- file.path("modules", module_name)
   rmd_file <- file.path(module_dir, paste0(module_name, ".Rmd"))
 
   if (!file.exists(rmd_file)) {
     stop("Quiz file not found: ", rmd_file)
+  }
+
+  # Copy all common files to module directory
+  common_files <- list.files(
+    "modules/common",
+    full.names = TRUE,
+    recursive = TRUE
+  )
+  copied_files <- character(0)
+  for (file in common_files) {
+    if (file.info(file)$isdir == FALSE) {
+      dest_file <- file.path(module_dir, basename(file))
+      file.copy(file, dest_file, overwrite = TRUE)
+      copied_files <- c(copied_files, dest_file)
+    }
   }
 
   # Deploy directly from module directory
@@ -20,6 +34,13 @@ deploy_quiz <- function(module_name) {
     logLevel = "verbose"
   )
   rsconnect::showLogs()
+
+  # Clean up: delete copied common files after deployment
+  for (file in copied_files) {
+    if (file.exists(file)) {
+      file.remove(file)
+    }
+  }
 }
 
 
